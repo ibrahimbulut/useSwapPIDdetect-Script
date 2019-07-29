92f9f67a3385167801fba24d83336bd9c7c0c300
ibrahimbulut <ibrahimbulut@users.noreply.github.com>
2019-07-29 16:57:45 +0300

    Update useSwapPIDdetect.sh

--- useSwapPIDdetect.sh
+++ useSwapPIDdetect.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#for i in `ls /proc`
-#do
-#	if [ -e /proc/$i/smaps ] # -e yerine -f de olabiir.
-#		then
-#		grep -H Swap /proc/$i/smaps | awk '( $2 != "0" ) { print }' 
-#	fi
-#done
-
-#!/bin/bash
-#!!!!!! awk -F '|' '{a[$1] += $3} END{print a["smiths"]}' filename.txt
-#!!!!!! awk -F '|' '{a[$1] += $3} END{for (i in a) print i, a[i]}' filename.txt
 for i in `ls /proc/ | sort -n | grep [0-9]`
 do
         if [ -e /proc/$i/smaps ] # -e yerine -f de olabiir.
@@ -18,5 +7,4 @@ do
               grep -H Swap /proc/$i/smaps | awk '( $2 != "0" )  { print }' >> swapKullanan.txt
         fi
 done
-#cat swapKullanan.txt | tr "/" " " | awk '{print $2 , $4}' | sort -n
 cat swapKullanan.txt | sed 's/\//:/g' | cut -d: -f3,6 | awk '{a[$1] += $2} END{for (i in a) print i,a[i],$3} ' | sort -n
